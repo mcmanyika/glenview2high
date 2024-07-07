ee7850998b5367814cf662b04c150bd690bbd0b7
Micah <developer@heartfeltonline.org>
2024-07-07 15:03:44 -0500

    added m-5 to images in clubs component

--- src/pages/clubs.js
+++ src/pages/clubs.js
@@ -10,7 +10,7 @@ const Clubs = () => {
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="relative flex items-center justify-center">
-              <div className="w-full h-96 relative overflow-hidden">
+              <div className="w-full h-96 m-5 relative overflow-hidden">
               <Image 
                   src="https://firebasestorage.googleapis.com/v0/b/glenview2-b3d45.appspot.com/o/general%2FScreen%20Shot%202024-07-03%20at%208.26.05%20AM.png?alt=media&token=ac93ba95-d1d1-4dc4-86df-c470cacd25c9" 
                   alt="Clubs 1" 
@@ -26,7 +26,7 @@ const Clubs = () => {
                 GlenView 2 High offers a wide range of student clubs and organizations where students can pursue their interests, develop leadership skills, and build lasting friendships. From academic clubs to cultural and sports clubs, there is something for everyone.
               </p>
             </div>
-            <div className="p-10 flex flex-col justify-center">
+            <div className="md:p-10 flex flex-col justify-center">
               <h2 className="text-4xl font-bold mb-4">Extracurricular Activities</h2>
               <p className="text-xl font-thin mb-4">
                 Our extracurricular activities enrich the student experience by providing opportunities for personal growth, creativity, and teamwork. Students can participate in events, competitions, community service projects, and more.

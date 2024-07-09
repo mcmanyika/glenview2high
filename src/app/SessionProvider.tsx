b967b1bdf46c33acb74180983952f7e65993c197
Micah <developer@heartfeltonline.org>
2024-07-09 15:03:33 -0500

    debug session

--- src/app/SessionProvider.tsx
+++ src/app/SessionProvider.tsx
@@ -3,14 +3,14 @@ import { Session } from 'next-auth';
 import { SessionProvider as Provider } from 'next-auth/react';
 
 type Props = {
-    children: React.ReactNode;
-    session: Session | null;
+  children: React.ReactNode;
+  session: Session | null;
 }
 
-export default function SessionProvider({children, session}: Props) {
+export default function SessionProvider({ children, session }: Props) {
   return (
-    <Provider>
-        {children}
+    <Provider session={session}>
+      {children}
     </Provider>
-  )
+  );
 }

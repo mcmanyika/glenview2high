c7d595e89b66ea0b269d9bb54ff3c30a061f2ae1
Micah <developer@heartfeltonline.org>
2024-07-09 00:13:58 -0500

    update

--- src/app/components/NewStudents.js
+++ src/app/components/NewStudents.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'; // Import Link from Next.js for navigation
 
 const NewStudents = () => {
   return (
@@ -11,9 +12,12 @@ const NewStudents = () => {
       <section className="relative text-white p-20 md:p-15 text-center">
         <h1 className="text-3xl md:text-3xl font-thin font-sans uppercase">Welcome New Students</h1>
         <p className="mt-4 text-base md:text-lg">
-          <button className="uppercase p-2 bg-blue-400 text-white  hover:bg-blue-500 transition duration-300">
-            Find out more
-          </button>
+          {/* Use Link component for navigation */}
+          <Link href="/admissions">
+            <button className="uppercase p-2 bg-blue-400 text-white hover:bg-blue-500 transition duration-300">
+              Find out more
+            </button>
+          </Link>
         </p>
       </section>
     </div>

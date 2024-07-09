c5d19397f9d8c4e9fba717d62470ea26a7c0067c
Micah <developer@heartfeltonline.org>
2024-07-09 14:54:10 -0500

    deleted index1

--- src/pages/index1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Layout from '../app/components/Layout'
-import '../app/globals.css';
-
-export default function index() {
-  return (
-    <Layout></Layout>
-  )
-}

f6019a3a9a15b0010e5c3a7c00cdd82f220c0175
Micah <developer@heartfeltonline.org>
2024-07-09 19:07:49 -0500

    update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,22 +3,22 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { ref, onValue, query, orderByChild, equalTo } from 'firebase/database';
 import { database } from '../../../utils/firebaseConfig';
-import { useGlobalState, setIsOverlayVisible } from '../store'; // Import useGlobalState and setIsOverlayVisible
+import { useGlobalState, setIsOverlayVisible } from '../store';
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Header = () => {
   const { data: session } = useSession();
   const [titles, setTitles] = useState([]);
   const [isSticky, setIsSticky] = useState(false);
-  const [isOpen, setIsOpen] = useState(false); // State to manage mobile menu visibility
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [isOverlayVisible] = useGlobalState('isOverlayVisible'); // Use global state hook
+  const [isOverlayVisible] = useGlobalState('isOverlayVisible');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const titleRef = ref(database, 'title'); // Reference to 'title' collection in Firebase
-        const statusQuery = query(titleRef, orderByChild('status'), equalTo('Active')); // Query to filter by status 'Active'
+        const titleRef = ref(database, 'title');
+        const statusQuery = query(titleRef, orderByChild('status'), equalTo('Active'));
 
         onValue(statusQuery, (snapshot) => {
           const data = snapshot.val();
@@ -31,31 +31,26 @@ const Header = () => {
                 status: data[key].status,
               }))
               .sort((a, b) => {
-                if (a.title === 'Admissions') return 1; // Move 'Admissions' to the end
-                if (b.title === 'Admissions') return -1; // Move 'Admissions' to the end
-                if (a.title === 'Alumni') return 1; // Move 'Alumni' to the end
-                if (b.title === 'Alumni') return -1; // Move 'Alumni' to the end
-                return a.title.localeCompare(b.title); // Sort other titles alphabetically
+                if (a.title === 'Admissions') return 1;
+                if (b.title === 'Admissions') return -1;
+                if (a.title === 'Alumni') return 1;
+                if (b.title === 'Alumni') return -1;
+                return a.title.localeCompare(b.title);
               });
             setTitles(titlesArray);
           } else {
-            setTitles([]); // Handle no data case
+            setTitles([]);
           }
         });
       } catch (error) {
         console.error('Firebase Error:', error);
-        // Handle error fetching data
       }
     };
 
     fetchData();
 
     const handleScroll = () => {
-      if (window.scrollY > 600) {
-        setIsSticky(true);
-      } else {
-        setIsSticky(false);
-      }
+      setIsSticky(window.scrollY > 600);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -65,26 +60,24 @@ const Header = () => {
     };
   }, []);
 
-  // Function to toggle mobile menu
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  // Function to toggle overlay visibility
   const toggleOverlay = () => {
     setIsOverlayVisible(!isOverlayVisible);
   };
 
   return (
     <header
-      className={`fixed z-50 w-full bg-blue-400  text-white transition-all duration-500 ease-in-out ${
-        isSticky ? 'top-0 ' : 'bottom-0 border-t-2 border-t-white p-5'
-      } ${!isSticky && 'hidden md:block'}`} // Hide on mobile screens when !isSticky
+      className={`fixed z-50 w-full bg-blue-400 text-white transition-all duration-500 ease-in-out ${
+        isSticky ? 'top-0' : 'bottom-0 border-t-2 border-t-white p-5'
+      } ${!isSticky && 'hidden md:block'}`}
     >
       {isSticky && (
         <div className='top-0 w-full text-white p-0 text-right'>
           <div className='container mx-auto text-xs p-2 mb-2'>
-            {session ? <span>Hi {session.user.name}</span> : <>Welcome Guest </>}, &nbsp; 
+            {session ? <span>Hi {session.user.name}</span> : <>Welcome Guest </>}, &nbsp;
             {session ? (
               <button onClick={() => signOut()}> Sign Out</button>
             ) : (
@@ -99,15 +92,16 @@ const Header = () => {
             <Image
               src="/images/logo.png"
               alt="Logo"
-              width={isSticky ? 50 : 70} // Adjusted width based on sticky state
-              height={isSticky ? 50 : 80} // Adjusted height based on sticky state
+              width={isSticky ? 50 : 70}
+              height={isSticky ? 50 : 80}
               className="rounded"
+              priority
             />
           </Link>
           <h1 className="text-sm md:text-2xl font-normal uppercase flex">GlenView 2 High</h1>
         </div>
         {isSticky && (
-          <div className="md:hidden"> {/* Display menu icon on mobile */}
+          <div className="md:hidden">
             <button onClick={toggleOverlay} className="text-white focus:outline-none">
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 {isOpen ? (
@@ -119,8 +113,8 @@ const Header = () => {
             </button>
           </div>
         )}
-        <ul className="hidden md:flex space-x-4"> {/* Hide on mobile, show on medium screens and above */}
-          {titles.map((rw) => (
+        <ul className="hidden md:flex space-x-4">
+          {titles.length > 0 && titles.map((rw) => (
             <li key={rw.id}>
               <Link href={`${rw.link}`}>
                 <div className="hover:text-gray-300 text-sm font-sans font-thin uppercase pb-2 border-b-2 border-transparent hover:border-blue-200">{rw.title}</div>

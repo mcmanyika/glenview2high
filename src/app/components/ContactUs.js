92823543dd1988214afcb7692a1640cfcd312330
Micah <developer@heartfeltonline.org>
2024-07-11 09:47:45 -0500

    update

--- src/app/components/ContactUs.js
+++ src/app/components/ContactUs.js
@@ -42,82 +42,83 @@ const ContactUs = () => {
   }
 
   return (
-    <div className=" bg-blue-50 p-10">
-      <div className='container mx-auto flex flex-col md:flex-row text-gray-500 font-thin '>
-      <div className="p-8 flex-1">
-        <h2 className="text-2xl mb-4">Contact Info</h2>
-        {/* <div className="w-full text-lg font-thin font-sans pb-5">
-        We are dedicated to providing quality education and fostering a supportive learning environment for our students.
-        </div> */}
-        <p className="mb-4 flex items-start">
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-4 mt-1" />
-          <span>
-            <strong>Address:</strong><br />
-            GlenView 2 High School,<br />
-            9480 1st Drive, Glenview 3,<br />
-            Harare, Zimbabwe
-          </span>
-        </p>
-        <p className="mb-4 flex items-start">
-          <FontAwesomeIcon icon={faPhone} className="mr-4 mt-1" />
-          <span>
-            <strong>Phone:</strong><br />
-            +263 123 456 789
-          </span>
-        </p>
-        <p className="mb-4 flex items-start">
-          <FontAwesomeIcon icon={faEnvelope} className="mr-4 mt-1" />
-          <span>
-            <strong>Email:</strong><br />
-            info@glenview2high.com
-          </span>
-        </p>
-      </div>
+    <div className="bg-blue-50 p-10">
+      <div className="container mx-auto flex flex-col md:flex-row text-gray-500 font-thin">
+        <div className="p-8 flex-1 flex flex-col  ">
+          <div className="w-full">
+            <h2 className="text-2xl mb-10">Contact Info</h2>
+            <div className='items-start justify-center'>
+            <div className="w-full mb-4 flex items-start justify-center">
+              <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-4 mt-1" />
+              <span>
+                <strong>Address:</strong><br />
+                GlenView 2 High School,<br />
+                9480 1st Drive, Glenview 3,<br />
+                Harare, Zimbabwe
+              </span>
+            </div>
+            <div className="w-full mb-4 flex items-start justify-center">
+              <FontAwesomeIcon icon={faPhone} className="mr-4 mt-1" />
+              <span>
+                <strong>Phone:</strong><br />
+                +263 123 456 789
+              </span>
+            </div>
+            <div className="w-full mb-4 flex items-start justify-center">
+              <FontAwesomeIcon icon={faEnvelope} className="mr-4 mt-1" />
+              <span>
+                <strong>Email:</strong><br />
+                info@glenview2high.com
+              </span>
+            </div>
+          </div>
+        </div>
+        </div>
 
-      <div className="flex-1">
-        <form onSubmit={handleSubmit} className="p-8 text-black w-full">
-          <h2 className="text-2xl mb-4">Contact Us</h2>
-          <div className="mb-4">
-            <label className="block text-sm font-bold mb-2" htmlFor="name">Name</label>
-            <input
-              id="name"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-              className="w-full p-2 border rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-bold mb-2" htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full p-2 border rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-bold mb-2" htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-              className="w-full p-2 border rounded"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Submit
-          </button>
-        </form>
-        <ToastContainer position="bottom-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
-      </div>
+        <div className="flex-1 flex flex-col justify-center">
+          <form onSubmit={handleSubmit} className="p-8 text-black w-full">
+            <h2 className="text-2xl mb-4">Contact Us</h2>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="name">Name</label>
+              <input
+                id="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="w-full p-2 border rounded"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full p-2 border rounded"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+                className="w-full p-2 border rounded"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </form>
+          <ToastContainer position="bottom-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+        </div>
       </div>
     </div>
   );

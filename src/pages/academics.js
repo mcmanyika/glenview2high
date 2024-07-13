2ac6810f6b82630349ac6acb91810511458ade19
Micah <developer@heartfeltonline.org>
2024-07-12 19:03:23 -0500

    update

--- src/pages/academics.js
+++ src/pages/academics.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Layout from '../app/components/Layout2';
 import Image from 'next/image';
-import Subjects from '../app/components/Subjects';
+import Subjects from '../app/components/Subjects'
 
 const Academics = () => {
   return (

92e5baf988a764f34b61f1c2f6b9f7996ecd1026
mcmanyika <mcmanyika@gmail.com>
2024-07-23 11:30:41 -0500

    added Murenga

--- src/app/components/Layout.js
+++ src/app/components/Layout.js
@@ -1,5 +1,8 @@
+// components/Layout.js
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import AIAssistantForm from '../components/ai/AIAssistantForm';
 
 const Layout = ({ children }) => {
   return (
@@ -9,6 +12,7 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <Footer />
+      <AIAssistantForm />
     </>
   );
 };

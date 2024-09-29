d3730565e735252de6f186f29fcb082fd6c7cb5d
mcmanyika <mcmanyika@gmail.com>
2024-09-28 22:19:51 -0500

    update

--- src/app/components/admin/blog/BlogPost.js
+++ src/app/components/admin/blog/BlogPost.js
@@ -5,8 +5,8 @@ const BlogPost = ({ title, category, createdAt, status }) => {
     <div className="p-2 bg-white  border-b mb-4">
       <h2 className="text-lg font-bold uppercase">{title}</h2>
       <div className="text-gray-600 mb-2">
-        <span className="mr-4">Category: {category}</span> <br/>
-        <span>Created At: {new Date(createdAt).toLocaleString()}</span>
+        <span className="text-sm mr-4">Category: {category}</span> <br/>
+        <span className='text-sm'>Created At: {new Date(createdAt).toLocaleString()}</span>
       </div>
       <div className="text-sm">
         <strong>Status:</strong> {status}

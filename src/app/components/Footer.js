7a96e5d8c43163b1a8c6dc6945779e883fc6d6ed
mcmanyika <mcmanyika@gmail.com>
2024-10-21 00:35:28 -0400

    link to smart learner

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,11 +1,13 @@
+import Link from "next/link";
 import Address from "./Address";
 
 const Footer = () => {
     return (
-      <footer className="bg-gray-400 text-white text-sm p-4">
+      <footer className="bg-footer text-white text-sm p-4">
         <Address />
         <div className="container mx-auto font-thin text-center">
-        &copy; Copyrights reserved {new Date().getFullYear()}. Developed by <b>LEAPFROG</b> 
+        &copy; Copyrights reserved {new Date().getFullYear()}. Developed by <Link href='https://smartlearner.vercel.app/' target="_blank"><b>LEAPFROG</b> 
+        </Link>
         </div>
       </footer>
     );

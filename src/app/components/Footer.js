a21e74abe449a1a6b514cb3f3b0c39b4ade57792
mcmanyika <mcmanyika@gmail.com>
2024-10-30 20:03:15 -0400

    UPdate

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="bg-gray-400 text-white text-sm p-4">
         <Address />
         <div className="container mx-auto font-thin text-center">
-        &copy; Copyrights reserved {new Date().getFullYear()}. Developed by <Link href='https://smartlearner.vercel.app/' target="_blank"><b>LEAPFROG</b> 
+        &copy; Copyrights reserved {new Date().getFullYear()}. Developed by <Link href='https://smartlearner.vercel.app/' target="_blank"><b>SMART LEARNER</b> 
         </Link>
         </div>
       </footer>
